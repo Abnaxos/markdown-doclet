be9991fcaa35c20b99eda710cc5c76d03ba5e5ae
Marko Umek <umek.marko@gmail.com>
2017-01-08 18:28:57 +0100

    Add GistMarkdownTagletClasspathLoader and use it instead of ClasspathLoader from Velocity.

---
+++ doclet/src/main/java/ch/raffael/doclets/pegdown/mdt/gist/GistMarkdownTagletClasspathLoader.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2013-2017 Raffael Herzog, Marko Umek
+ *
+ * This file is part of pegdown-doclet.
+ *
+ * pegdown-doclet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pegdown-doclet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pegdown-doclet.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package ch.raffael.doclets.pegdown.mdt.gist;
+
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+
+/**
+ * The {@link ClasspathResourceLoader} for the {@link GistMarkdownTaglet}.
+ */
+@SuppressWarnings("unused")
+public class GistMarkdownTagletClasspathLoader extends ClasspathResourceLoader {
+}
